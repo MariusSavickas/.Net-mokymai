ea69474b93275453ffa3d32b1b9c9e182a2d81dc
Marius Savickas <marius.savickas@codeacademylt.onmicrosoft.com>
2022-06-06 21:24:38 +0300

    update0

--- Basic mokymai/Basic mokymai/Program.cs
+++ Basic mokymai/Basic mokymai/Program.cs
@@ -18,7 +18,33 @@
             Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eilutėje"); //kompozicija
             Console.WriteLine($"{"išvedimas "}{"vienoje "}{"eilutėje "}"); //interpoliacija
             Console.WriteLine("--------------------------");
-        
+
+
+            Console.WriteLine("tekstas įdedamas į \"kabutes\" ");
+            Console.WriteLine($"tekstas {Environment.NewLine}\nįdedamas \r\nį \n\"kabutes\" ");
+            Console.WriteLine("tekstas \tįdedamas \tį \t\"kabutes\" ");
+
+
+            Console.WriteLine("-----Press any key to continue------");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Console.WriteLine("Įveskite savo vardą, o aš jį pakartosiu");
+            //Console.WriteLine("o štai mano pakartojimas: " + Console.ReadLine());
+            //Console.WriteLine("o štai mano pakartojimas: {0} ",  Console.ReadLine());
+            //Console.WriteLine($"o štai mano pakartojimas: {Console.ReadLine()}");
+
+            //Console.WriteLine("Įveskitę raidę ");
+            //var key = Console.ReadKey();
+            //Console.WriteLine("Įvestas simbolis {0}", key.KeyChar);
+            //Console.WriteLine("Įvestas simbolis {0}", key.Key);
+            //Console.WriteLine("Įvestas simbolis {0}", (int)key.KeyChar);
+
+            //Console.WriteLine("Įvestas simbolis {0}", Console.ReadKey().KeyChar);
+            //Console.WriteLine("Įvestas simbolis {0}", (int)Console.ReadKey().KeyChar);
+
+            Console.WriteLine("Įveskite savo vardą, o aš atspėsiu pirmą raidę ");
+            Console.WriteLine("o štai mano pakartojimas:" + Console.ReadLine()[0]);
 
 
 
