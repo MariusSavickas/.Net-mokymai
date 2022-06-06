ce6b6d4878baf424789b0bbedd11709468a2af22
Marius Savickas <marius.savickas@codeacademylt.onmicrosoft.com>
2022-06-06 20:17:48 +0300

    paaiškinta string konkatinacija, kompozicija ir interpoliacija

--- Basic mokymai/Basic mokymai/Program.cs
+++ Basic mokymai/Basic mokymai/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.Write("išvedimas ");
+            Console.Write("vienoje ");
+            Console.Write("eiluteje ");
+            Console.WriteLine();
+            Console.WriteLine("tekstas kitoje eilutėje");
+            Console.Write("tekstas");
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("išvedimas "+ "vienoje " + "eilutėje "); //konkatinacija
+            Console.WriteLine("{0} {1} {2}", "išvedimas", "vienoje", "eilutėje"); //kompozicija
+            Console.WriteLine($"{"išvedimas "}{"vienoje "}{"eilutėje "}"); //interpoliacija
+            Console.WriteLine("--------------------------");
+        
+
+
+
+            Console.ReadKey();
         }
     }
 }
