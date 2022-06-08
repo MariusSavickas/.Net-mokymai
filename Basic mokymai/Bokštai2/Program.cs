4f647a71ec3cfdc7d0e56076388cb46ec42d6b7a
Marius Savickas <marius.savickas@codeacademylt.onmicrosoft.com>
2022-06-08 18:13:36 +0300

    Bokštas2

--- Basic mokymai/Bokštai2/Program.cs
+++ Basic mokymai/Bokštai2/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Tower of Honoi");
 
+
+            //Kintamieji
             string stulpas1 = "----Stulp1----";
             string stulpas2 = "----Stulp2----";
             string stulpas3 = "----Stulp3----";
@@ -26,7 +28,7 @@
             string lekste4 = "   ****|****";
             string lekste43 = "     ****|****";
 
-            //1
+            //1 užduotis
             Console.WriteLine("\n1 uzduotis nupieškite Tower of Hanoi. Piešiniui naudokite kintamuosius.\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + lekste1 + stul2 +stul2);
@@ -37,7 +39,7 @@
             Console.WriteLine("\n----tęsti----\n");
             Console.WriteLine(Console.ReadLine());
 
-            //2
+            //2 užduotis
             Console.WriteLine("2 uzduotis Apverskite pirmą stulpelį ir išveskite visą Tower of Hanoi\n");
             Console.WriteLine(eile1 + lekste4 + stul2 + stul2);
             Console.WriteLine(eile2 + lekste3 + stul2 + stul2);
@@ -49,7 +51,7 @@
             Console.WriteLine(Console.ReadLine());
 
 
-            //3
+            //3 užduotis
             Console.WriteLine("3 uzduotis Išvalykite pirmą stulpelį ir išveskite tuščią Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + stul1 + stul2 + stul2);
@@ -60,7 +62,7 @@
             Console.WriteLine("\n----tęsti----\n");
             Console.WriteLine(Console.ReadLine());
 
-            //4
+            //4 užduotis
             Console.WriteLine("4 uzduotis Į kiekvieno stulpelio 5eil įdėkite po 4 dalių elementą ir išveskite Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + stul1 + stul2 + stul2);
@@ -71,7 +73,7 @@
             Console.WriteLine("\n----tęsti----\n");
             Console.WriteLine(Console.ReadLine());
 
-            //5
+            //5 užduotis
             Console.WriteLine("5uzduotis Į 1stulp 5eil įdėkite 4 dalių elementą, 2sutup 5eil - 3 dalių, 3sutup 4eil - 1 dalies, 3sutup 5eil - 2 dalių,ir išveskite Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + stul1 + stul2 + stul2);
@@ -82,7 +84,7 @@
             Console.WriteLine("\n----tęsti----");
             Console.WriteLine(Console.ReadLine());
 
-            //6
+            //6 užduotis
             Console.WriteLine("6. užduotis Į 1stulp 4eil įdėkite tokį pat elementą kaip yra 3stup 4eil, ir išveskite Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + stul1 + stul2 + stul2);
@@ -93,7 +95,7 @@
             Console.WriteLine("\n----tęsti----");
             Console.WriteLine(Console.ReadLine());
 
-            //7
+            //7 užduotis
             Console.WriteLine("7. užduotis Į visas 2stulp eilutes įdėkite tokį pat elementą kaip yra 3stup 5eil, ir išveskite Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + lekste23 + stul2);
             Console.WriteLine(eile2 + stul1 + lekste23 + stul2);
@@ -104,7 +106,7 @@
             Console.WriteLine("\n----tęsti----\n");
             Console.WriteLine(Console.ReadLine());
 
-            //8
+            //8 užduotis
             Console.WriteLine("8. užduotis į 3stulp sudėkite teisingą piramidę. 1stulp ir 2 stulp turi likti tušti, išveskite Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + stul1 + stul2 + lekste13);
@@ -115,7 +117,7 @@
             Console.WriteLine("\n----tęsti----\n");
             Console.WriteLine(Console.ReadLine());
 
-            //9
+            //9 užduotis
             Console.WriteLine("9.užduotis pakeiskite visų elementų dizainą iš # į , išveskite Tower of Hanoi\n");
             Console.WriteLine(eile1 + stul1 + stul2 + stul2);
             Console.WriteLine(eile2 + stul1 + stul2 + lekste13.Replace("*", "\""));
@@ -126,7 +128,21 @@
             Console.WriteLine("\n----tęsti----\n");
             Console.WriteLine(Console.ReadLine());
 
-
+            
+            //10 uzduotis užduotis
+            Console.WriteLine("paprašykite naudotojo nupiešti 1 sulpelio 1 eilutę. Išveskite visą Tower of Hanoi su perpiešta pirma eilute\n");
+            Console.WriteLine("\nNupieškite 1 stulpelio 1 eilutę, o aš ją atkartosiu\n");
+            
+            Console.WriteLine(eile1 + Console.ReadLine()[1] + stul1 + stul2 + stul2);
+            Console.WriteLine(eile2 + stul1 + stul2 + lekste13.Replace("*", "\""));
+            Console.WriteLine(eile3 + stul1 + stul2 + lekste23.Replace("*", "\""));
+            Console.WriteLine(eile4 + stul1 + stul2 + lekste33.Replace("*", "\""));
+            Console.WriteLine(eile5 + stul1 + stul2 + lekste43.Replace("*", "\""));
+            Console.WriteLine(eile6 + stulpas1 + stulpas2 + stulpas3);
+            Console.WriteLine("\n----tęsti----\n");
+            Console.WriteLine(Console.ReadLine());
+            
+            
 
         }
     }
